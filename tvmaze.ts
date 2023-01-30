7b9cbf8f35e3d1e750f00d0722448f179d1b4339
michaelpappas <pappas.michael@gmail.com>
2023-01-30 13:27:43 -0800

    up to part 4 in exercise

--- tvmaze.ts
+++ tvmaze.ts
@@ -4,7 +4,23 @@ import * as $ from 'jquery';
 const $showsList = $("#showsList");
 const $episodesArea = $("#episodesArea");
 const $searchForm = $("#searchForm");
+const $episodesBtn = $(".Show-getEpisodes")
 
+
+interface MovieInterface {
+  show:{ id: number;
+    name: string;
+    summary: string,
+    image: {medium: string, original: string}
+  }
+}
+
+interface EpisodeInterface {
+  id: number,
+  name: string,
+  season: number,
+  number: number
+}
 
 /** Given a search term, search for tv shows that match that query.
  *
@@ -13,45 +29,30 @@ const $searchForm = $("#searchForm");
  *    (if no image URL given by API, put in a default image URL)
  */
 
-async function getShowsByTerm(term) {
+async function getShowsByTerm(term:string | number | string[] | undefined):Promise<Array<MovieInterface>> {
+  const response = await axios.get(`https://api.tvmaze.com/search/shows?q=${term}`)
   // ADD: Remove placeholder & make request to TVMaze search shows API.
-  return [
-    {
-      id: 1767,
-      name: "The Bletchley Circle",
-      summary:
-        `<p><b>The Bletchley Circle</b> follows the journey of four ordinary
-           women with extraordinary skills that helped to end World War II.</p>
-         <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their
-           normal lives, modestly setting aside the part they played in
-           producing crucial intelligence, which helped the Allies to victory
-           and shortened the war. When Susan discovers a hidden code behind an
-           unsolved murder she is met by skepticism from the police. She
-           quickly realises she can only begin to crack the murders and bring
-           the culprit to justice with her former friends.</p>`,
-      image:
-          "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
-    }
-  ]
+  console.log(response)
+  return response.data
 }
 
 
 /** Given list of shows, create markup for each and to DOM */
 
-function populateShows(shows) {
+function populateShows(shows:Array<MovieInterface>): void {
   $showsList.empty();
-
+console.log(shows)
   for (let show of shows) {
     const $show = $(
-        `<div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
+        `<div data-show-id="${show.show.id}" class="Show col-md-12 col-lg-6 mb-4">
          <div class="media">
            <img
-              src="http://static.tvmaze.com/uploads/images/medium_portrait/160/401704.jpg"
+              src=${show.show.image.medium}
               alt="Bletchly Circle San Francisco"
               class="w-25 me-3">
            <div class="media-body">
-             <h5 class="text-primary">${show.name}</h5>
-             <div><small>${show.summary}</small></div>
+             <h5 class="text-primary">${show.show.name}</h5>
+             <div><small>${show.show.summary}</small></div>
              <button class="btn btn-outline-light btn-sm Show-getEpisodes">
                Episodes
              </button>
@@ -76,18 +77,31 @@ async function searchForShowAndDisplay() {
   populateShows(shows);
 }
 
+
+
 $searchForm.on("submit", async function (evt) {
   evt.preventDefault();
   await searchForShowAndDisplay();
 });
+
+$episodesBtn.on("click", async function (evt){
+  evt.preventDefault()
+  const showId = $(evt.target).closest(".Show").data("show-id")
+  const episodes = await getEpisodesOfShow(showId)
+  populateEpisodes(episodes)
+})
+
+
 
 
 /** Given a show ID, get from API and return (promise) array of episodes:
  *      { id, name, season, number }
  */
 
-// async function getEpisodesOfShow(id) { }
+async function getEpisodesOfShow(id:number):Promise<Array<EpisodeInterface>> {
+  const episodes = await axios.get(`https://api.tvmaze.com/search/shows?q=${term}`)
+ }
 
 /** Write a clear docstring for this function... */
 
-// function populateEpisodes(episodes) { }
+function populateEpisodes(episodes) { }
